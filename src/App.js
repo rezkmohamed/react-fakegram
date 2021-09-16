dd310bace49c3b0a1ac115d60eeb9daa7b3f7c0e
rezkmohamed <momito310399@gmail.com>
2021-09-16 15:08:42 +0200

    added SearchPage component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router';
 import './App.css';
 import Homepage from './components/pages/Homepage/Homepage';
 import ProfilePage from './components/pages/ProfilePage/ProfilePage';
+import SearchPage from './components/pages/SearchPage/SearchPage';
 import Header from './components/UI/Header';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Route path="/" exact component={Homepage}/>
       <Route path="/profilepage" exact component={ProfilePage} />
+      <Route path="/searchprofile" exact component={SearchPage} />
     </React.Fragment>
   );
 }
---
+++ src/components/pages/SearchPage/SearchPage.js
@@ -0,0 +1,81 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import classes from "./SearchPage.module.css";
+import globalClasses from "../../../assets/global-styles/bootstrap.min.module.css";
+
+
+const SearchPage = (props) => {
+    return(
+        <div className={`${globalClasses.container} ${classes.container}`}>
+            <div>
+                <div className={`${globalClasses.media} ${classes.media}`}>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png" 
+                    className={globalClasses['mr-3']} alt="..." width="64px" height="64px" />
+                    <div className={globalClasses['media-body']}>
+                        <Link 
+                            className={`${classes['username-link']} ${globalClasses['username-link']}`}>
+                            
+                        <h5 className={globalClasses['mt-0']}>profilo.nickname</h5>
+                        </Link>
+                        biografia del profilo test
+                    </div>
+                </div>
+
+                <div className={`${globalClasses.media} ${classes.media}`}>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png" 
+                    className={globalClasses['mr-3']} alt="..." width="64px" height="64px" />
+                    <div className={globalClasses['media-body']}>
+                        <Link 
+                            className={`${classes['username-link']} ${globalClasses['username-link']}`}>
+                            
+                        <h5 className={globalClasses['mt-0']}>profilo.nickname</h5>
+                        </Link>
+                        biografia del profilo test
+                    </div>
+                </div>
+
+                <div className={`${globalClasses.media} ${classes.media}`}>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png" 
+                    className={globalClasses['mr-3']} alt="..." width="64px" height="64px" />
+                    <div className={globalClasses['media-body']}>
+                        <Link 
+                            className={`${classes['username-link']} ${globalClasses['username-link']}`}>
+                            
+                        <h5 className={globalClasses['mt-0']}>profilo.nickname</h5>
+                        </Link>
+                        biografia del profilo test
+                    </div>
+                </div>
+
+                <div className={`${globalClasses.media} ${classes.media}`}>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png" 
+                    className={globalClasses['mr-3']} alt="..." width="64px" height="64px" />
+                    <div className={globalClasses['media-body']}>
+                        <Link 
+                            className={`${classes['username-link']} ${globalClasses['username-link']}`}>
+                            
+                        <h5 className={globalClasses['mt-0']}>profilo.nickname</h5>
+                        </Link>
+                        biografia del profilo test
+                    </div>
+                </div>
+
+                <div className={`${globalClasses.media} ${classes.media}`}>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png" 
+                    className={globalClasses['mr-3']} alt="..." width="64px" height="64px" />
+                    <div className={globalClasses['media-body']}>
+                        <Link 
+                            className={`${classes['username-link']} ${globalClasses['username-link']}`}>
+                            
+                        <h5 className={globalClasses['mt-0']}>profilo.nickname</h5>
+                        </Link>
+                        biografia del profilo test
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+}
+
+export default SearchPage;
