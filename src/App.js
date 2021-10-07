eb44be9b2a1caf3dbb0cade24ad4ca32e97e5ec9
rezkmohamed <momito310399@gmail.com>
2021-10-07 15:54:04 +0200

    added auth guard

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Route } from 'react-router';
+import React, { useContext } from 'react';
+import { Redirect, Route } from 'react-router';
 import './App.css';
 import AddNewPost from './components/pages/AddNewPost/AddNewPost';
 import Login from './components/pages/auth/login/Login';
@@ -8,18 +8,54 @@ import DetailPostPage from './components/pages/DetailPostPage/DetailPostPage';
 import Homepage from './components/pages/Homepage/Homepage';
 import ProfilePage from './components/pages/ProfilePage/ProfilePage';
 import SearchPage from './components/pages/SearchPage/SearchPage';
+import AuthContext from './services/auth-context';
 
 
 function App() {
+  const authCtx = useContext(AuthContext);
+
   return (
     <React.Fragment>
-      <Route path="/" exact component={Homepage}/>
-      <Route path="/login" exact component={Login} />
-      <Route path="/register" exact component={Register} />
-      <Route path="/profiles/:idProfile" exact component={ProfilePage} />
-      <Route path="/searchprofile" exact component={SearchPage} />
-      <Route path="/posts/detail/:idPost" exact component={DetailPostPage} />
-      <Route path="/newpost" exact component={AddNewPost} />
+      {
+        authCtx.isLoggedIn &&
+        <Route path="/" exact component={Homepage}/>
+      }
+      {
+        !authCtx.isLoggedIn &&
+        <Route path="/login" exact component={Login} />
+      }
+      {
+        !authCtx.isLoggedIn &&
+        <Route path="/register" exact component={Register} />
+      }
+      {
+        authCtx.isLoggedIn &&
+        <Route path="/profiles/:idProfile" exact component={ProfilePage} />
+      }
+      {
+        authCtx.isLoggedIn &&
+        <Route path="/searchprofile" exact component={SearchPage} />
+      }
+      {
+        authCtx.isLoggedIn &&
+        <Route path="/posts/detail/:idPost" exact component={DetailPostPage} />
+      }
+      {
+        authCtx.isLoggedIn &&
+        <Route path="/newpost" exact component={AddNewPost} />
+      }
+      {     
+        authCtx.isLoggedIn &&
+        <Route path='*'>
+          <Redirect to='/' />
+        </Route>
+      }
+      {
+        !authCtx.isLoggedIn &&
+        <Route path='*'>
+          <Redirect to='/login' />
+        </Route>
+      }
     </React.Fragment>
   );
 }
