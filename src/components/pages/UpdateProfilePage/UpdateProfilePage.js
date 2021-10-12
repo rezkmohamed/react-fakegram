dce530fe1c9724ba22b3dfb444e32c190dcf9034
rezkmohamed <momito310399@gmail.com>
2021-10-12 11:41:20 +0200

    refactor style UpdateProfilePage

--- src/components/pages/UpdateProfilePage/UpdateProfilePage.js
+++ src/components/pages/UpdateProfilePage/UpdateProfilePage.js
@@ -8,70 +8,70 @@ const UpdateProfilePage = () => {
     return(
         <React.Fragment>
             <Header />
-            <div class="container">
-                <div class="row">
-                    <div class="col-3"></div>
-                    <div class="col-6">
-                        <div class="loading">loading...</div>
+            <div className="container">
+                <div className={`${classes.row} ${globalClasses.row}`}>
+                    <div className="col-3"></div>
+                    <div className="col-6">
+                        {/* <div className="loading">loading...</div>
 
-                        <div class="alert alert-danger" role="alert">
+                        <div className="alert alert-danger" role="alert">
                         Errore nella modifica dei dati!
                         </div>
 
-                        <div class="alert alert-success" role="alert">
+                        <div className="alert alert-success" role="alert">
                             Dati cambiati con successo
-                        </div>
+                        </div> */}
 
                         <form enctype="multipart/form-data">
                             <h3>Nickname:</h3>
                             <input 
                                     type="text"
-                                    class="form-control"
+                                    className="form-control"
                                     name="nickname"
                                     required
                                     minlength="4" />
                             <h3>Nome:</h3>
                             <input 
                                     type="text"
-                                    class="form-control"
+                                    className="form-control"
                                     name="nome"
                                     required
                                     minlength="4" />
                             <h3>Biografia:</h3>
                             <input 
                                     type="text" 
-                                    class="form-control"
+                                    className="form-control"
                                     name="biografia"
                                     required
                                     minlength="4" />
                             <button 
                                     type="submit"
-                                    class="btn btn-primary">
+                                    className={`btn btn-primary ${classes['btn-primary']}`}>
                                 modifica
                             </button>
                         </form>
                     </div>
-                    <div class="col-3"></div>
+                    <div className="col-3"></div>
                 </div>
 
                 <hr />
-                {/* <div class="row">
-                        <div class="col-3"></div>
-                        <div class="col-6">
-                            <div class="alert alert-danger" role="alert"
+                {/* <div className="row">
+                        <div className="col-3"></div>
+                        <div className="col-6">
+                            <div className="alert alert-danger" role="alert"
                             *ngIf="!fileIsOkay && fileIsSended ">
                             Errore nel caricamento della foto!
                             </div>
                 
-                            <div class="alert alert-success" role="alert" 
+                            <div className="alert alert-success" role="alert" 
                             *ngIf="fileIsOkay && fileIsSended">
                                 Foto caricata con successo!
                             </div>
                             <h3>Seleziona un'immagine del profilo: (formato png)</h3>
-                            <div class="custom-file mb-2">
+                            <div className="custom-file mb-2">
                                 <input 
                                     type="file"
-                                    class="custom-file-input"
+                                    className="custom-file-input"
                                     name="proPic"
                                     id="customFile"
                                     #fileInput
@@ -79,32 +79,31 @@ const UpdateProfilePage = () => {
                                     required
                                     >
                                 <label 
-                                    class="custom-file-label"
+                                    className="custom-file-label"
                                     for="customFile"
                                     (click)="fileInput.click()">
                                     <span *ngIf="!fileIsSelected">Scegli file</span>
                                     <span *ngIf="fileIsSelected">{{fileName}}</span>
                             </label>
-                                <button class="btn btn-primary">Carica la foto</button>
+                                <button className="btn btn-primary">Carica la foto</button>
 
                             </div>
 
                         </div>
-                        <div class="col-3"></div>
+                        <div className="col-3"></div>
                 </div> */}
 
 
-                <hr />
-                <div class="row">
-                    <div class="col-3"></div>
-                    <div class="col-6">
-                        <div class="alert alert-danger" role="alert">
+                <div className="row">
+                    <div className="col-3"></div>
+                    <div className="col-6">
+                        {/* <div className="alert alert-danger" role="alert">
                                 Errore nel cambio email, ricontrolla la password!
                         </div>
 
-                        <div class="alert alert-success" role="alert">
+                        <div className="alert alert-success" role="alert">
                             Email cambiata con successo
-                        </div>
+                        </div> */}
 
                         <form>
                             <h3>email:</h3>
@@ -112,41 +111,41 @@ const UpdateProfilePage = () => {
                                     type="text"
                                     id="email"
                                     formControlName="email"
-                                    class="form-control" />
+                                    className="form-control" />
                             <h3>password:</h3>
                             <input
                                     type="password"
                                     id="password1"
                                     formControlName="password"
-                                    class="form-control" />
+                                    className="form-control" />
                             <h3>confirm password:</h3>
                             <input
                                     type="password"
                                     id="password-confirm1"
                                     formControlName="confirm"
-                                    class="form-control" />
+                                    className="form-control" />
                             <button
                                     type="submit"
-                                    class="btn btn-primary">
+                                    className={`btn btn-primary ${classes['btn-primary']}`}>
                                     cambia mail
                             </button>
                         </form>
                     </div>
-                    <div class="col-3"></div>
+                    <div className="col-3"></div>
                 </div>
 
                 <hr />
-                <div class="row">
-                    <div class="col-3"></div>
-                    <div class="col-6">
+                <div className="row">
+                    <div className="col-3"></div>
+                    <div className="col-6">
 
-                        <div class="alert alert-danger" role="alert">
+                        {/* <div className="alert alert-danger" role="alert">
                         password sbagliata!
                         </div>
 
-                        <div class="alert alert-success" role="alert">
+                        <div className="alert alert-success" role="alert">
                             Password modificata con successo.
-                        </div>
+                        </div> */}
 
                         
                         <form>
@@ -155,28 +154,28 @@ const UpdateProfilePage = () => {
                                     type="password"
                                     id="old"
                                     formControlName="old"
-                                    class="form-control" />
+                                    className="form-control" />
                             <h3>nuova password:</h3>
                             <input 
                                     type="password"
                                     id="new"
                                     formControlName="new"
-                                    class="form-control" />
+                                    className="form-control" />
                             <h3>conferma password:</h3>
                             <input 
                                     type="password"
                                     id="confirm"
                                     formControlName="confirm"
-                                    class="form-control" />
+                                    className="form-control" />
                             <button 
                                     
                                     type="submit"
-                                    class="btn btn-primary">
+                                    className={`btn btn-primary ${classes['btn-primary']}`}>
                                 cambia password
                             </button>
                         </form>
                     </div>
-                    <div class="col-3"></div>
+                    <div className="col-3"></div>
                 </div>
             </div>
         </React.Fragment>
