4e590f83e07b41b9360d40523f6a975a419b4855
rezkmohamed <momito310399@gmail.com>
2021-11-10 09:36:41 +0100

    added fetchPendingQuestions API

--- src/services/question-service.js
+++ src/services/question-service.js
@@ -19,4 +19,26 @@ export const fetchQuestionsForProfile = (idProfile) => {
     }
 
     return fetchQuestionsForProfileReq();
-}
+};
+
+export const fetchPendingQuestions = () => {
+    const token = localStorage.getItem('token');
+    const fetchPendingQuestionsReq = async () => {
+        const response = await fetch(urlBase + "pending", {
+            headers: {
+                'Authorization': 'Bearer ' + token,
+            }
+        });
+
+        if(!response.ok){
+            console.log('error: ' + response.status);
+            throw new Error('Error: ' + response.status);
+        }
+        const data = await response.json();
+
+        return data;
+    }
+
+    return fetchPendingQuestionsReq();
+};
+
