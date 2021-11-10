37ae556c10ae2944da6457e7adfc03d56fb1eaa6
rezkmohamed <momito310399@gmail.com>
2021-11-10 15:06:35 +0100

    refactor QuestionPendingCard

--- src/components/UI/QuestionPendingCard.js
+++ src/components/UI/QuestionPendingCard.js
@@ -18,6 +18,10 @@ const QuestionPendingCard = ({question}) => {
                     !question.isAnonym &&
                     <h1 className="display-4"><Link to={"/profiles/" + question.profileSender.id} style={{ textDecoration: 'none', color: '#ff2c74' }}>{question.profileSender.nickname}</Link> ha chiesto</h1>
                 }
+                {
+                    question.isAnonym &&
+                    <h1 className="display-4">un anonimo ha chiesto</h1>
+                }
                 <p className="lead">{question.question}</p>
                 <button className={`btn btn-primary btn-lg ${classes.color}`}onClick={goToAnswerPage}>Rispondi</button>
             </div>
