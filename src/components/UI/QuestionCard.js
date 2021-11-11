7ff86e95f7281bfb5484fcc33ec507147ed8e797
rezkmohamed <momito310399@gmail.com>
2021-11-11 12:01:29 +0100

    added delete question's logic

--- src/components/UI/QuestionCard.js
+++ src/components/UI/QuestionCard.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import classes from "./QuestionCard.module.scss";
+import { setQuestionToPending } from '../../services/question-service';
 
 const QuestionCard = ({question}) => {
+
+    const hideQuestion = () => {
+        const questionToHide = document.getElementById(`${question.idQuestion}`);
+        console.log(questionToHide);
+        questionToHide.style["display"] = "none";
+    }
+
     const onRemoveQuestion = () => {
         const isSure = window.confirm('SICURO?');
         console.log(isSure);
+        hideQuestion();
         if(isSure){
-            
+            setQuestionToPending(question.idQuestion)
+            .then(res => {
+                if(res) {
+                    hideQuestion();
+                }
+            }).catch(err => {
+                window.alert('ERRROOORRRR:::: ' + err.message);
+            })
         }
     };  
 
     return (
         <React.Fragment>
-            <div className={`jumbotron ${classes['main-div']}`}>
+            <div className={`jumbotron ${classes['main-div']}`} id={question.idQuestion} > 
                 <button className="btn btn-danger float-right" onClick={onRemoveQuestion}>x</button>
                 {
                     !question.isAnonym && 
