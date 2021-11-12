929b508aafc5249c75a7eb9b2d344b3b07ad5dd3
rezkmohamed <momito310399@gmail.com>
2021-11-12 15:11:52 +0100

    refactor notifications page's style

--- src/components/pages/NotificationsPage/NotificationsPage.js
+++ src/components/pages/NotificationsPage/NotificationsPage.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from '../../UI/Header';
 import Notification from '../../UI/Notification';
+import classes from './NotificationsPage.module.scss';
 
 const NotificationsPage = (props) => {
     return (
         <React.Fragment>
             <Header />
-            <div>
+            <div className={classes['notifications-container']}>
                 <Notification />
                 <Notification />
                 <Notification />
