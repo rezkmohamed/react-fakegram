3e8c31ff5aa2fb4b2571df4d9729ddfb609af9f3
rezkmohamed <momito310399@gmail.com>
2021-11-15 11:40:31 +0100

    added fetch notifications request in NotificationsPage

--- src/components/pages/NotificationsPage/NotificationsPage.js
+++ src/components/pages/NotificationsPage/NotificationsPage.js
@@ -1,20 +1,51 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from '../../UI/Header';
 import Notification from '../../UI/Notification';
 import classes from './NotificationsPage.module.scss';
+import { fetchNotifications } from '../../../services/notification-service';
+import { notificationsActions } from '../../../store/notifications-slice';
+import { useDispatch } from 'react-redux';
+import { useState } from 'react/cjs/react.development';
 
 const NotificationsPage = (props) => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [notifications, setNotifications] = useState([]);
+    const dispatch = useDispatch();
+
+
+    useEffect(() => {
+        setIsLoading(true);
+        fetchNotifications()
+        .then(res => {
+            setNotifications(res);
+            console.log(res);
+            dispatch(notificationsActions.setNotifications(res));
+            setIsLoading(false);
+        }).catch(err => {
+            window.alert('ERRORREEEEE::::: ' + err.message);
+            setIsLoading(false);
+        })
+    }, []);
+
     return (
         <React.Fragment>
             <Header />
             <div className={classes['notifications-container']}>
+                {
+                    isLoading && 
+                    <h1>LOADINGGGGG</h1>
+                }
+                {
+                    !isLoading &&
+                    <Notification />
+                }
+                {/* <Notification />
                 <Notification />
                 <Notification />
                 <Notification />
                 <Notification />
                 <Notification />
-                <Notification />
-                <Notification />
+                <Notification /> */}
             </div>
         </React.Fragment>
     );
