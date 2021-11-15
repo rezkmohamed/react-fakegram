289deb1a16b979ca03c8f891f462130783344edf
rezkmohamed <momito310399@gmail.com>
2021-11-15 14:33:57 +0100

    refactor Notification component

--- src/components/UI/Notification.js
+++ src/components/UI/Notification.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Notification = ({notification}) => {
+
     return (
         <React.Fragment>
             {
                 notification && 
-                <div class="media" >
-                    <img src={notification.profileNotificator.proPic} width="64px" height="64px" class="mr-3" alt="immgggg" />
-                    <div class="media-body">
-                        <h5 class="mt-0 notification-type">nickname notificatore tipo di notifica</h5>
+                <div className="media">
+                    <img src={notification.profileNotificator.proPic} width="64px" height="64px" className="mr-3" alt="immgggg" />
+                    <div className="media-body">
+                        <h5 className="mt-0 notification-type"><Link to={notification.link} style={{ color: 'inherit', textDecoration: 'inherit' }}>{notification.profileNotificator.nickname}</Link> {notification.notificationMessage}</h5>
                         <p >MESSAGGIO DELLA NOTIFICA</p>
                     </div>
                 </div>
--- src/services/notification-service.js
+++ src/services/notification-service.js
@@ -2,6 +2,29 @@ import noProPic from "../assets/no-pro-pic.png";
 
 const urlBase = 'http://localhost:8080/notifications/';
 
+
+const transformNotificationObj = (notification) => {
+    if(!notification.profileNotificator.proPic) {
+        notification.profileNotificator.proPic = noProPic;
+    }
+    switch(notification.notificationType){
+        case "LIKE":
+            notification.notificationMessage = "ha messo like al post";
+            notification.link = `/posts/detail/${notification.idPost}`;
+            break;
+        case "COMMENT":
+            notification.notificationMessage = "ha commentato il tuo post";
+            notification.link = `/posts/detail/${notification.idPost}`;
+            break;
+        case "FOLLOW":
+            notification.notificationMessage = "ha iniziato a seguirti";
+            notification.link = `/profiles/${notification.profileNotificator.id}`;
+            break;
+        default: 
+            return;
+    }
+};
+
 export const fetchNotifications = () => {
     const token = localStorage.getItem('token');
     const fetchNotificationsReq = async () => {
@@ -17,9 +40,7 @@ export const fetchNotifications = () => {
         }
         const data = await response.json();
         for(let notification of data){
-            if(!notification.profileNotificator.proPic) {
-                notification.profileNotificator.proPic = noProPic;
-            }
+            transformNotificationObj(notification);
         }
 
         return data;
